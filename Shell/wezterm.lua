611d4dc83f89e13ce175c0acb63272071be3eee6
edothas <spaceplant2@gmail.com>
2024-11-27 12:17:54 -0500

    more wezterm work

--- Shell/wezterm.lua
+++ Shell/wezterm.lua
@@ -10,6 +10,20 @@ local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
 local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
 local is_windows <const> = wezterm.target_triple:find("windows") ~= nil
 
+-- for background images
+if is_linux then
+  bg_image = '/home/edothas/Pictures/1257089-3709430.jpg'
+end
+if is_windows then
+  -- bg_image = 'C:\\Users\\mreed\\OneDrive - Mueller Water Products\\Pictures\\194427-outer-space-background-image.jpg'
+  bg_image = "C:\\Users\\mreed\\OneDrive - Mueller Water Products\\Pictures\\309938-alita-battle-angel-background.jpg"
+end
+local dimmer = {
+  brightness = 0.15,
+  hue = 1.0,
+  saturation = 1.0,
+}
+
 if is_linux then
   config.default_prog = { '/usr/bin/bash' }
 end
@@ -27,7 +41,6 @@ config.show_tab_index_in_tab_bar = true
 config.tab_and_split_indices_are_zero_based = false
 config.show_new_tab_button_in_tab_bar = true
 config.unzoom_on_switch_pane = true
-
 -- Tab padding
 config.window_padding = {
   left = 2,
@@ -36,26 +49,16 @@ config.window_padding = {
   bottom = 0,
 }
 
-config.window_frame = {
--- The font used in the tab bar.
--- Whatever font is selected here, it will have the main font setting appended to it to pick up any
--- fallback fonts you may have used there.
-  font = wezterm.font_with_fallback { 
--- Linux fonts
-    'eufm10',
-    'Chilanka',
--- Windows fonts
-    'Ink Free'
-  },
-  font_size = 12.0,
--- The overall background color of the tab bar when the window is focused
-  active_titlebar_bg = '#553333',
--- The overall background color of the tab bar when the window is not focused
-  inactive_titlebar_bg = '#553333',
-}
+if is_windows then
+  config.window_frame = {
+    font = wezterm.font { family = 'Ink Free' },
+    font_size = 12.0,
+    active_titlebar_bg = '#333333',
+    inactive_titlebar_bg = '#333333',
+  }
+end
 config.colors = {
 	tab_bar = {
--- The color of the inactive tab bar edge/divider
 		inactive_tab_edge = '#575757',
 	},
 }
@@ -65,76 +68,64 @@ config.inactive_pane_hsb = {
 	brightness = 0.8,
 }
 --  font, background, and color scheme
+if is_linux then
+  config.font = wezterm.font_with_fallback {
+    'Ubuntu Mono',
+  }
+end
 
-config.font = wezterm.font_with_fallback {
--- linux fonts
-  'Ubuntu Mono',
--- windows fonts
+if is_windows then
+  config.font = wezterm.font_with_fallback {
 	'Lucida Console',
 	'Consolas',
 	'Cascadia Code',
 	'Symbol',
-}
+  }
+end
+
+config.color_scheme = 'Eldritch'
+-- config.color_scheme = 'aikofog'
 -- config.color_scheme = 'Alien Blood (Gogh)'
-config.color_scheme = 'aikofog'
 -- config.color_scheme = 'Dark Pastel (Gogh)'
 -- config.color_scheme = 'darkmoss'
-config.text_background_opacity = 0.2
---  static background image --
--- config.window_background_image = '/home/edothas/Pictures/aurora-1938348.jpg'
--- config.window_background_image_hsb = {
--- 	brightness = 0.1,
--- 	hue = 1.0,
--- 	saturation = 1.0,
--- }
-
---  A more complex background that scrolls
-local dimmer = {
-  brightness = 0.1,
-  hue = 1.0,
-  saturation = 1.0,
-}
+-- config.text_background_opacity = 0.2
 
 config.enable_scroll_bar = true
 config.min_scroll_bar_height = '2cell'
 config.colors = {
   scrollbar_thumb = 'white',
 }
+-- if is_linux then
 config.background = {
-  -- This is the deepest/back-most layer. It will be rendered first
   {
-    source = {
-      File = '/home/edothas/Pictures/1257089-3709430.jpg',
-    },
-    -- The texture tiles vertically but not horizontally.
-    -- When we repeat it, mirror it so that it appears "more seamless".
-    -- An alternative to this is to set `width = "100%"` and have
-    -- it stretch across the display
-    repeat_x = 'Mirror',
-    hsb = dimmer,
-    -- When the viewport scrolls, move this layer 10% of the number of
-    -- pixels moved by the main viewport. This makes it appear to be
-    -- further behind the text.
-    attachment = { Parallax = 0.1 },
-    width = '100%',
-    repeat_y_size = '100%',
+      source = {
+        File = bg_image,
+      },
+      repeat_x = 'Mirror',
+      repeat_y = 'Mirror',
+      hsb = dimmer,
+      width = '100%',
+      attachment = { Parallax = 0.1 },
+    }
   }
-}
--- 
+-- end
 --  set the working environment
 config.default_cwd = ""
-config.launch_menu = {
-	{
-		args = { 'top' },
-	},
-	{
-		label = 'Powershell',
-		args = { 'powershell.exe', '-nologo' },
-	},
-	{
-		label = 'CMD',
-		args = { 'cmd.exe' },
-	}
-}
+if is_windows then
+  config.launch_menu = {
+    {
+      args = { 'top' },
+    },
+    {
+      label = 'Powershell',
+      args = { 'powershell.exe', '-nologo' },
+    },
+    {
+      label = 'CMD',
+      args = { 'cmd.exe' },
+    }
+  }
+end
+
 -- and finally, return the configuration to wezterm
 return config
